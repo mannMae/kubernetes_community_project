f7595151dc7b783ae4f38e975254daba9837ea44
Jaeman Kim <daga4242@gmail.com>
2023-10-27 21:02:55 +0900

    move to login button

--- frontend/src/components/Layout/SideMenu.jsx
+++ frontend/src/components/Layout/SideMenu.jsx
@@ -2,11 +2,12 @@ import { Button } from 'components/Elements';
 import { Wrapper } from './SideMenu.style';
 import { authSlice, logout } from 'features/auth';
 import { useNavigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 export const SideMenu = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
+  const auth = useSelector(({ auth }) => auth);
 
   const handleClickLogout = async () => {
     await logout()
@@ -16,9 +17,17 @@ export const SideMenu = () => {
       .then((res) => navigate('/'))
       .catch((error) => console.error(error));
   };
+  console.log(auth);
+  if (auth?.credential) {
+    return (
+      <Wrapper>
+        <Button onClick={() => handleClickLogout()}>로그아웃</Button>
+      </Wrapper>
+    );
+  }
   return (
     <Wrapper>
-      <Button onClick={() => handleClickLogout()}>로그아웃</Button>
+      <Button onClick={() => navigate('/auth/login')}>로그인하기</Button>
     </Wrapper>
   );
 };
